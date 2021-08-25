6d3014ca0d2159461a7c93f386d739e3b941672d
surajjayraman <suraj.jayraman@gmail.com>
2021-08-24 20:14:56 -0400

    animated sentence implementation

--- typewriter.js
+++ typewriter.js
@@ -1,10 +1,17 @@
 const sentence = "hello there from lighthouse labs";
-
-for (let i = 0; i < sentence.length; i++) {
+let i = 0;
+const typewriter = () => {
   setTimeout(() => {
-    // print the char here
-    console.log(sentence[i]);
-  }, 1000); // <= 1s delay to make it noticeable. Can dial it down later.
-  
-  
-}
+    process.stdout.write(sentence[i]);
+    i += 1;
+    if (i < sentence.length) {
+      typewriter();
+    }
+    if (i === sentence.length) {
+      process.stdout.write('\n');
+    }
+
+  }, 50);
+
+};
+typewriter();
